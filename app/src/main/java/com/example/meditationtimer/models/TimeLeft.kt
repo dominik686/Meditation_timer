f25873275bedfac6d2ad9fb1ef19975931190f26
Domininiuk <allorify@gmail.com>
2021-12-12 06:01:22 +0100

    Time is now displayed in a "Minutes : Seconds" format, not sure if I should make it "Hours : Minutes : Seconds" when needed

--- app/src/main/java/com/example/meditationtimer/models/TimeLeft.kt
+++ app/src/main/java/com/example/meditationtimer/models/TimeLeft.kt
@@ -18,4 +18,23 @@ data class TimeLeft(var hours : Int, var minutes : Int, var seconds : Int)
         minutes = seconds / 60
         seconds -= minutes * 60
     }
+
+    override fun toString(): String {
+        if(hours < 0)
+        {
+            if(seconds < 10)
+            {
+                // Show a zero for seconds  eg. "4:05 instead of 4:5
+                return "$hours:$minutes:0$seconds"
+            }
+            return "$hours:$minutes:$seconds"
+        }
+
+        if(seconds < 10)
+        {
+            // Show a zero for seconds  eg. "4:05 instead of 4:5
+            return "$minutes:0$seconds"
+        }
+        return "$minutes:$seconds"
+    }
 }
--- app/src/test/java/com/example/meditationtimer/models/TimeLeftTest.kt
+++ app/src/test/java/com/example/meditationtimer/models/TimeLeftTest.kt
@@ -5,12 +5,70 @@ import org.junit.Test
 import org.junit.Assert.*
 class TimeLeftTest : TestCase() {
 
+
+
     @Test
-    fun test_convertSecondsToMinutes_3600seconds_returnsTrue()
+    fun test_convertSecondsToMinutes_minutesEqualToSixty_returnsTrue()
     {
         var timeLeft = TimeLeft(3600)
 
         assertEquals(60, timeLeft.minutes)
     }
 
+    @Test
+    fun test_convertSecondsToMinutes_secondsEqualToZero_returnsTrue()
+    {
+        var timeLeft = TimeLeft(3600)
+
+        assertEquals(0, timeLeft.seconds)
+    }
+
+    @Test
+    fun test_convertSecondsToMinutes_hoursEqualToZero_returnsTrue()
+    {
+        //Refactor the method to also convert minutes to hours?
+        var timeLeft  = TimeLeft(3600)
+
+        assertEquals(0, timeLeft.hours)
+    }
+
+    @Test
+    fun test_convertSecondsToMinutes_minutesEqualToFive_returnsTrue()
+    {
+        var timeLeft = TimeLeft(300)
+
+        assertEquals(5,timeLeft.minutes )
+    }
+
+    @Test
+    fun test_convertSecondsToMinutes_secondsEqualToFive_returnsTrue()
+    {
+        var timeLeft = TimeLeft(300)
+
+        assertEquals(0,timeLeft.seconds)
+    }
+
+
+
+
+    @Test
+    fun test_toString_SixtyMinutesZeroSeconds()
+    {
+        var timeLeft = TimeLeft(3600)
+
+        assertEquals("60 : 0", timeLeft.toString())
+    }
+
+    @Test
+    fun test_toString_OneHourZeroMinutesZeroSeconds()
+    {
+
+    }
+    @Test
+    fun test_toString_MinusFiveMinutesZeroSeconds()
+    {
+        var timeLeft = TimeLeft(-3600)
+
+        assertEquals("-60 : 0", timeLeft.toString())
+    }
 }
