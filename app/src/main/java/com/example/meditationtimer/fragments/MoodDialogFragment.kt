ac44a4a3a751f6a9be35382d66b3f5ac220bc3a6
Domininiuk <allorify@gmail.com>
2022-02-25 17:20:39 +0100

    moods icons are clicakble now

--- app/src/main/java/com/example/meditationtimer/fragments/MoodDialogFragment.kt
+++ app/src/main/java/com/example/meditationtimer/fragments/MoodDialogFragment.kt
@@ -2,41 +2,34 @@ package com.example.meditationtimer.fragments
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
-import com.daimajia.androidanimations.library.Techniques
-import com.daimajia.androidanimations.library.YoYo
 import com.example.meditationtimer.R
 import android.view.View
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 
 
 class MoodDialogFragment : DialogFragment()
 {
     private lateinit var builder : AlertDialog.Builder
 
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val inflater = this.layoutInflater
-
         val dialogView: View = inflater.inflate(R.layout.mood_dialog_fragment, null)
 
         builder = AlertDialog.Builder(activity)
-
-
-
-        val but = dialogView.findViewById(R.id.neutral_emoji) as ImageButton
-        but.setOnClickListener{
-            YoYo.with(Techniques.Shake)
-                .duration(700)
-                .repeat(5)
-                .playOn(it);
-        }
-
-
+        builder.setMessage("Choose a number")
+        builder.setTitle("Choose your mood emoji:")
         builder.setView(dialogView)
+        val but = dialogView.findViewById(R.id.neutral_emoji) as ImageButton
 
+        but.setOnClickListener  {
+            but.isPressed = true }
         builder.create().show()
         return super.onCreateDialog(savedInstanceState)
     }
