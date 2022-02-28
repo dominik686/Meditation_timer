3ef6c917a31b03751a418c3c5873ab48002fc988
Domininiuk <allorify@gmail.com>
2022-02-28 06:53:08 +0100

    Changed imagebuttons in MoodDialog to Chips

--- app/src/main/java/com/example/meditationtimer/fragments/MoodDialogFragment.kt
+++ app/src/main/java/com/example/meditationtimer/fragments/MoodDialogFragment.kt
@@ -8,29 +8,82 @@ import androidx.fragment.app.DialogFragment
 import com.example.meditationtimer.R
 import android.view.View
 import android.view.LayoutInflater
-import android.widget.Button
 import android.widget.ImageButton
-import android.widget.Toast
+
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import com.google.android.material.chip.Chip
 
 
 class MoodDialogFragment : DialogFragment()
 {
     private lateinit var builder : AlertDialog.Builder
 
+    lateinit var animContext : Context
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        animContext = context
+    }
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val inflater = this.layoutInflater
-        val dialogView: View = inflater.inflate(R.layout.mood_dialog_fragment, null)
-
         builder = AlertDialog.Builder(activity)
-        builder.setMessage("Choose a number")
-        builder.setTitle("Choose your mood emoji:")
-        builder.setView(dialogView)
-        val but = dialogView.findViewById(R.id.neutral_emoji) as ImageButton
+        builder.setTitle("Describe your mood:")
+         //builder.setView(dialogView)
 
-        but.setOnClickListener  {
-            but.isPressed = true }
-        builder.create().show()
+        //builder.create().show()
         return super.onCreateDialog(savedInstanceState)
     }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view: View = inflater.inflate(R.layout.mood_dialog_fragment, null)
+
+        builder.setView(view)
+
+        val neutralChip = view.findViewById(R.id.neutral_chip) as Chip
+
+        neutralChip.setOnClickListener  {
+            bounce(it)
+        }
+
+
+        val badChip = view.findViewById(R.id.bad_chip) as Chip
+
+        badChip.setOnClickListener  {
+            bounce(it)
+        }
+
+        val veryBadChip = view.findViewById(R.id.very_bad_chip) as Chip
+
+        veryBadChip.setOnClickListener  {
+            bounce(it)
+        }
+
+        val goodChip = view.findViewById(R.id.good_chip) as Chip
+
+        goodChip.setOnClickListener  {
+            bounce(it)
+        }
+        val greatChip = view.findViewById(R.id.great_chip) as Chip
+
+        greatChip.setOnClickListener  {
+            bounce(it)
+        }
+
+
+
+        builder.create().show()
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    fun bounce(view : View)
+    {
+       val animation = AnimationUtils.loadAnimation(animContext, R.anim.bounce)
+        view.startAnimation(animation)
+    }
+
 }
